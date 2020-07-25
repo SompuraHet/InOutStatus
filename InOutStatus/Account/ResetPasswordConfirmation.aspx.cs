8ca7da45dd9a286759c757ee9488eca71d7b1df0
Jalpa Patel <200395023@student.georgianc.on.ca>
2020-07-25 14:39:26 -0400

    Add project files.

---
+++ InOutStatus/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace InOutStatus.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ InOutStatus/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InOutStatus.Startup))]
+namespace InOutStatus
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
