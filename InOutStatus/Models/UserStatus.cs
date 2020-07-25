9e60d3d4158f5a5b23676a464827462d0a17f3b0
Jalpa Patel <200395023@student.georgianc.on.ca>
2020-07-25 14:56:02 -0400

    Model UserStatus

---
+++ InOutStatus/Models/UserStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InOutStatus.Models
+{
+    public class UserStatus
+    {
+        public int Id { get; set; }
+
+        public string WorkStatus { get; set; }
+
+        public string Location { get; set; }
+
+        public string Comment { get; set; }
+
+        public string QuickSet { get; set; }
+    }
+}
