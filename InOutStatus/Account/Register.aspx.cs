a61e0dac21dd4cf2b3fe384fa58e9feaf7bf3ad5
Jalpa Patel <200395023@student.georgianc.on.ca>
2020-07-25 14:48:16 -0400

    Created User Roles

--- InOutStatus/Account/Register.aspx.cs
+++ InOutStatus/Account/Register.aspx.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using InOutStatus.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace InOutStatus.Account
 {
@@ -24,6 +25,13 @@ namespace InOutStatus.Account
                 //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
                 //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
 
+                //TempCode to Create new Role
+                //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                //var roleManager = new RoleManager<IdentityRole>(roleStore);
+                //roleManager.Create(new IdentityRole("Users"));
+
+
+                manager.AddToRole(user.Id, "Users");
                 signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
             }
